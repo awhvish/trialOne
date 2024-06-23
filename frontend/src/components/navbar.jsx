8fbd4d1f48b98be99515d14db8b647c4d6c7668b
root <max@kali>
2024-06-23 21:36:15 +0530

    navbar functionalities

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
-import './css/Navbar.css';
+import { useState, useEffect } from "react";
+import React from "react";
+import "./css/Navbar.css";
 
 function Navbar() {
-    function handleSignout() {
-        alert('You are signed out.');
-        localStorage.removeItem('token');
+  const [checkLogin, setCheckLogin] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setCheckLogin(true);
+    } else {
+      setCheckLogin(false);
     }
+  }, []);
+
+  function handleSignout() {
+    alert("You are signed out.");
+    localStorage.removeItem("token");
+    setCheckLogin(false);
+  }
   return (
     <nav className="navbar">
       <div className="navbar-logo">trialOne</div>
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/signup">Signup</a></li>
-        <li><a href="/play">Play</a></li>
-        <li><a href="/" onClick={handleSignout}>Signout</a></li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        {!checkLogin && (
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        )}
+
+        <li>
+          <a href="/signup">Signup</a>
+        </li>
+        <li>
+          <a href="/play">Play</a>
+        </li>
+        {checkLogin && (
+          <li>
+            <a href="/" onClick={handleSignout}>
+              Signout
+            </a>
+          </li>
+        )}
       </ul>
     </nav>
   );
