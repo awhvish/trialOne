e3172c3354211dd5dc20973ed2838d260d82cec4
Avis Lowha <avis.mitblr2023@learner.manipal.edu>
2024-06-24 14:51:03 +0530

    api config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'http://13.49.21.122:4200',
     },
   },
   plugins: [react()],
